ddd8d5ecb80dc59329164209d7462e64d39830e5
Cristian <crisemcon@gmail.com>
2020-10-02 22:39:55 -0300

    log funcionando con el comando: log

--- README.md
+++ README.md
@@ -10,12 +10,21 @@ TO-DO
 (c) (X)  Debe ejecutar el comando ingresado en un proceso concurrente, para lo cual debe usar el llamado a sistema fork() y algunas de las variantes de exec(). Los comandos a soportar son ejecutados en foreground, es decir, la shell ejecuta y espera por el termino de su ejecucion antes de imprimir el promtp para esperar por el siguiente comando. //Por ejemplo: sleep 3
 
 (d) (X)  Si  se  presiona  “enter“  sin  previo  comando,  la  shell  simplemente  imprime  nuevamente  el prompt.
+
 (e) (_)  Debe proporcionar la salida de la ejecucion del comando en distinto color.
-(f) (_)  Su shell debe crear un comando que le permita crear un archivo mishell.log con el registro de los comandos ejecutados por sesion. Este comando puede ir agregando la informacion de distintas sesiones al final del existente o puede ser reescrito.
+
+(f) (X)  Su shell debe crear un comando que le permita crear un archivo mishell.log con el registro de los comandos ejecutados por sesion. Este comando puede ir agregando la informacion de distintas sesiones al final del existente o puede ser reescrito.  //El log es guardado con el comando: log    , los cambios son visualizados tras salir de la terminal
+
 (g) (X)  Su shell ademas debe soportar el comando exit para salir de la shell. 
+
 (h) (X?)  Debe poder continuar si es que un comando ingresado no existe, proporcionando el error correspondiente. //No especifica el error, solo muestra "Comando Desconocido"
+
 (i) (_)  Debe manejar procesos en background (comandos que se terminan con caracter &). Esto esque la shell no espere por el termino del comando, de manera que la shell pueda continuar ejecutando otros comandos. En este caso,  su shell debe registrar estos procesos de tal manera que si el usuario quiera terminar la shell, ́esta le proporcione la lista de procesos activos en background. Notar que aca no se pide hacer que un proceso background pase a foreground.
+
 (j) (_)  Manejar una cola de prioridades con los comandos mas frequentemente utilizados.  Asuma que tiene un maximo de k, cuyo valor se configura con un comando especial de su shell1.
+
 (k) (_)  Desplegar por pantalla los comandos en su cola de prioridades.
+
 (l) (_)  Elegir un comando de la lista y volver a ejecutarlo.
+
 (m) (_)  Finalmente, investigue lo que son los procesos defuncty agregue a su shell un comando que cree N procesos defunct y un comando que permita eliminarlos.
--- shell.c
+++ shell.c
@@ -17,7 +17,7 @@ void parse(char* input, char** params) { //Dividimos el input en un arreglo de p
     return;
 };
 
-int execute(char** params) {
+int execute(char** params, char* log) {
     pid_t pid = fork(); //Fork
 
     if (pid == -1) { //Error
@@ -26,27 +26,64 @@ int execute(char** params) {
     } else if (pid == 0) { // Proceso hijo
         execvp(params[0], params); //Ejecuta comando
         printf("Comando Desconocido\n");
-        return 0;
+        _exit(127); //Este exit es necesario para saber si fallo el execvp
+        return 1;
     } else { // Proceso padre
-        int child;
-        waitpid(pid, &child, 0); //Espera a que termine el hijo
+        int status;
+        int wait = waitpid(pid, &status, 0);
+        if (wait > 0) {
+            if (WIFEXITED(status) && !WEXITSTATUS(status)) { //Si se cumple el execvp termino bien
+                return 0;
+            } else { //El excecvp termino mal
+                return 1;
+            }
+        }
         return 1;
     }
 };
 
 int main() {    
     char input[MAX_INPUT_LENGTH+1];    
-    char * params[MAX_N_PARAMS+1];     
+    char * params[MAX_N_PARAMS+1];
+
+    int currentSize = 1;
+    char *success = " Success\n\0"; //Mensaje de comando satisfactorio en el log
+    char *failed = " Failed\n\0"; //Mensaje de comando erroneo en el log
+    char *log = malloc(sizeof(char)); //Creamos el log, el cual es un string dinamico
+    FILE *fptr; //Puntero a tipo archivo
+    fptr = fopen("mishell.log","a"); //Abrimos el archivo en modo append (para escribir sobre lo que existe), de no existir es creado
+    if (fptr == NULL)
+    { 
+        printf("Error abriendo el archivo log"); 
+        return 0; 
+    } 
+
     while(1) {
         printf("CustomUnixShell $:"); //Prompt   
         if(fgets(input, sizeof(input), stdin) == NULL) break; //Input vacio con CTRL+D cierra la shell
         if(input[0] == '\n') continue; //Si se presiona enter sin un comando previo, imprime denuevo la shell
         if(input[strlen(input)-1] == '\n') { // Se remueve el caracter de salto de linea al final del input 
             input[strlen(input)-1] = '\0';    
-        }    
-        parse(input, params); //Dividimos el input en un arreglo de parametros        
+        }
+        currentSize += strlen(input); //Aumentamos el tamano del string log segun el largo del input
+        log = realloc(log, currentSize * sizeof(char));
+        strcat(log, input); // Concatenamos el input al log
+        parse(input, params); //Dividimos el input en un arreglo de parametros  
+        if (!strcmp(params[0], "log")){ //Se crea el archivo de log con los datos recopilados 
+            fprintf(fptr,"%s\n",log); //Guardamos el log en el archivo
+            continue;
+        }     
         if (strcmp(params[0], "exit") == 0) break; // Comando exit  
-        if (execute(params) == 0) break;
-    } // end while
+        if (execute(params, log) == 0) { //Comando correcto
+            currentSize += strlen(success);//Aumentamos el tamano del string log segun el largo del string success
+            log = realloc(log, currentSize * sizeof(char));
+            strcat(log,success);
+        } else { //Comando incorrectos
+            currentSize += strlen(failed);//Aumentamos el tamano del string log segun el largo del string success
+            log = realloc(log, currentSize * sizeof(char));
+            strcat(log,failed);
+        }
+    } 
+    fclose(fptr); //Cerramos el archivo y se mostraran los cambios realizados en el
     return 0;
 }
